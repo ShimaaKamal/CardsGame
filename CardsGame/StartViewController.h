cc71996816664737d9b093d9bd3ee74bcdc7a8f4
Mohamed ELAhmady <mohamedzayyan23@gmail.com>
2015-04-12 11:46:10 +0200

    first version of the splash @Marco there is a condition that checks the user defaults it conflicts with the splash function i commented it to test it works fine i will improve it and work on the ui of different components but i  need some help to finish a nice ui before delivery thanks, El Ahmady

--- CardsGame/StartViewController.h
+++ CardsGame/StartViewController.h
@@ -1,5 +1,12 @@
 #import <UIKit/UIKit.h>
 
-@interface StartViewController : UIViewController
+@interface StartViewController : UIViewController{
+IBOutlet UIImageView *animationView;
+IBOutlet UIImageView *loadView;
+}
+-(void)delay1;
+-(void)delay2;
+-(void)delay3;
+
 
 @end
